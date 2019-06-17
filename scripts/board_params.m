2a3d53bcf183b34e97bb4ddc071a3435aa405afe
Kacper <Kacper@192.168.1.19>
2019-06-17 02:08:29 +0200

    add board and update control

---
+++ scripts/board_params.m
@@ -0,0 +1,34 @@
+box_model_params;
+border_width_row = 0.01; % m
+border_width_col = 0.005; % m
+n_rows = 8;
+n_cols = 7;
+box_size_with_wall = 2*box_size_wall_width + box_size_b;
+total_x = (box_size_with_wall + 2*border_width_row)*n_rows;
+total_y = (box_size_a + 2*border_width_col)*n_cols;
+list_of_coordinates_x = zeros(8,1);
+list_of_coordinates_y = zeros(7,1);
+list_of_coordinates_x(1) = 0; %border_width_row+box_size_with_wall/2;
+list_of_coordinates_y(1) = border_width_col+box_size_a/2;
+for i=2:n_rows
+    list_of_coordinates_x(i) = list_of_coordinates_x(i-1) + border_width_row+box_size_with_wall/2;
+end
+for i=2:n_cols
+    list_of_coordinates_y(i) = list_of_coordinates_y(i-1) +  border_width_col+box_size_a/2;
+end
+
+base_rail_lenth = total_x*100; % cm
+base_rail_height = 5; % cm
+base_rail_width = 5; % cm
+
+first_arm_length = 5; % cm
+first_arm_height = 50; % cm
+first_arm_width = 5; % cm
+
+second_arm_length = 50; % cm
+second_arm_height = 5; % cm
+second_arm_width = 5; % cm
+
+third_arm_length = 5; % cm
+third_arm_height = total_x*100 + 20; % cm
+third_arm_width = 5; % cm
---
+++ scripts/box_model_params.m
@@ -0,0 +1,12 @@
+box_size_a = 0.06; % m
+box_size_b = 0.03; % m
+box_size_c = 0.045; % m
+
+box_size_wall_width = 0.002; % m
+
+box_size_density = 500; % kg/m^3
+box_size_medium_density = 2200; % kg/m^3
+
+box_opacity = 0.8;
+box_color = [0.8706    0.9098    0.9529];
+box_medium_color = [0.8392    0.7961    0.4196];
